d42a0946574a1a0d4e3fd10de202cb3024b7f211
Blake Tarter <blake@codekoalas.com>
2016-09-01 23:34:21 -0500

    looks more like a puppy

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 'use strict';
 let message = process.argv[2] || 'woof';
 
-console.log(`                                    /-` + message.split('').map(() => '-').join('') + `-\\`);
-console.log(`                                    | ` + message + ` |`);
-console.log(`                                    \\_` + message.split('').map(() => '_').join('') + `_/`);
-console.log(`                           __      / /`);
-console.log(`     ,                   ," e\`--o   / `);
-console.log(`    ((                  (  | __,'  /  `);
-console.log(`     \\\\~---------------' \\_;/      `);
-console.log(`     (                     /          `);
-console.log(`     /) .______________.  )           `);
-console.log(`    (( (              (( (            `);
-console.log(`     \`\`-'              \`\`-'       `);
+console.log(`                               /-` + message.split('').map(() => '-').join('') + `-\\`);
+console.log(`                               | ` + message + ` |`);
+console.log(`                               \\_` + message.split('').map(() => '_').join('') + `_/`);
+console.log(`                      __      / /`);
+console.log(`     ,              ," e\`--o   / `);
+console.log(`    ((             (  | __,'  /  `);
+console.log(`     \\\\~----------' \\_;/      `);
+console.log(`     (                /          `);
+console.log(`     /) ._________.  )           `);
+console.log(`    (( (         (( (            `);
+console.log(`     \`\`-'         \`\`-'       `);
